4f38c1127766f2b489e3b7423368553dc952e9c8
GlebDontsov <dontsovggwp@mail.ru>
2023-04-02 17:39:58 +0300

    add db

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/BurntSushi/toml"
+	"github.com/joho/godotenv"
 	"http-rest-api/internal/app/apiserver"
 	"log"
+	"os"
 )
 
 var (
@@ -13,13 +16,20 @@ var (
 
 func init() {
 	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 	flag.Parse()
-
 	config := apiserver.NewConfig()
 	_, err := toml.DecodeFile(configPath, config)
+
+	passwordDB, exists := os.LookupEnv("DATABASE_PASSWORD")
+	if exists {
+		config.Store.DatabaseURL += fmt.Sprintf(" password=%s", passwordDB)
+	}
 
 	if err != nil {
 		log.Fatal(err)
